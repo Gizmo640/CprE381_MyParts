15ecd3b6216885517257d37a9b01b732e15187fa
dchism03 <dchism03@co2050-15.ece.iastate.edu>
2023-10-05 15:29:29 -0500

    finished barrel shifter

--- MyParts/Barrel Shifter/Barrel_Shifter.vhd
+++ MyParts/Barrel Shifter/Barrel_Shifter.vhd
@@ -7,9 +7,7 @@ entity Barrel_Shifter is
 	port(
 		shiftAmount : in std_logic_vector(4 downto 0); 
 		BarrelInput : in std_logic_vector(31 downto 0); 
-		Load_barrelInput : in std_logic; --what are the load ports for?
-		Load_selectLines : in std_logic;
-		Load_BarrelOutput : std_logic;
+		LeftOrRight : in std_logic; --0 is right and 1 is left
 		BarrelOutput : out std_logic_vector(31 downto 0));
 
 	end Barrel_Shifter;
@@ -18,10 +16,12 @@ entity Barrel_Shifter is
 architecture structure of Barrel_Shifter is 
 	
 	--signals
+	signal s_MuxLorR	: std_logic_vector(31 downto 0);
 	signal stage5MuxOutput : std_logic_vector(31 downto 0);
 	signal stage4MuxOutput : std_logic_vector(31 downto 0);
 	signal stage3MuxOutput : std_logic_vector(31 downto 0);
 	signal stage2MuxOutput : std_logic_vector(31 downto 0);
+	signal stage1MuxOutput : std_logic_vector(31 downto 0);
 	
 
 
@@ -36,12 +36,24 @@ architecture structure of Barrel_Shifter is
 
 	
 begin
+
+--CHOOSES RIGHT OR LEFT SHIFT (NORMAL INPUT OR REVERSED INPUT)
+	LEFT_OR_RIGHT: for i in 31 downto 0 generate
+		MuxLorR1: mux2t1
+			port map(
+				i_D0	=>	BarrelInput(i),
+				i_D1	=>  BarrelInput(31-i),
+				i_S		=>  LeftOrRight,
+				o_0		=>  s_MuxLorR(i)
+			);
+			end generate LEFT_OR_RIGHT;
+
 --STAGE 5
 	--bits 31-16 set to 0
 	Stage5Upper: for i in 31 downto 16 generate
 			MuxCascade: mux2t1
 					port map(
-							i_D0 => BarrelInput(i),
+							i_D0 => s_MuxLorR(i), --need a signal
 							i_D1 => '0',
 							i_S => shiftAmount(4),
 							o_0 => stage5MuxOutput(i)
@@ -52,8 +64,8 @@ begin
 	Stage5Lower: for i in 15 downto 0 generate
 			MuxCascade: mux2t1
 					port map(
-							i_D0 => BarrelInput(i),
-							i_D1 => BarrelInput(i+16),
+							i_D0 => s_MuxLorR(i),
+							i_D1 => s_MuxLorR(i+16),
 							i_S => shiftAmount(4),
 							o_0 => stage5MuxOutput(i)
 					);
@@ -135,7 +147,7 @@ begin
 							i_D0 => stage2MuxOutput(31),
 							i_D1 => '0',
 							i_S => shiftAmount(0),
-							o_0 => BarrelOutput(31)
+							o_0 => stage1MuxOutput(31)
 					);
 	--loop from 30 downto 0 for all shifted bit values
 	Stage1Lower: for i in 30 downto 0 generate
@@ -144,7 +156,19 @@ begin
 							i_D0 => stage2MuxOutput(i),
 							i_D1 => stage2MuxOutput(i+1),
 							i_S => shiftAmount(0),
-							o_0 => BarrelOutput(i)
+							o_0 => stage1MuxOutput(i)
 					);
 					end generate Stage1Lower;
+
+--CHOOSES RIGHT OR LEFT SHIFT (NORMAL INPUT OR REVERSED INPUT)
+	LEFT_OR_RIGHT_OUTPUT: for i in 31 downto 0 generate
+		MuxLorR2: mux2t1
+			port map(
+				i_D0	=>	stage1MuxOutput(i),
+				i_D1	=>  stage1MuxOutput(31-i),
+				i_S		=>  LeftOrRight,
+				o_0		=>  BarrelOutput(i)
+			);
+			end generate LEFT_OR_RIGHT_OUTPUT;
+
 end structure;
