6bb93155d9edcfdec35ee0a63418ef937a45b3dc
lincolnh <lincolnh@iastate.edu>
2023-10-18 14:35:07 -0500

    commented

--- OurParts/ALU/ALU_ControlUnit.vhd
+++ OurParts/ALU/ALU_ControlUnit.vhd
@@ -3,7 +3,7 @@ use IEEE.std_logic_1164.all;
 
 entity ALU_ControlUnit is
 	port(
-		ALU_ControlUnit_In: in STD_LOGIC_VECTOR(&* downto 0)
+		ALU_ControlUnit_In: in STD_LOGIC_VECTOR(3 downto 0)
 		AddSubtract_Signal_Out: out STD_LOGIC;
 		LogicSelect_Signal_Out: out STD_LOGIC_VECTOR(1 downto 0);
 		InvertSelect_Signal_Out: out STD_LOGIC;
@@ -34,3 +34,20 @@ begin
 	ArithmeticLogicSelect_Signal_Out <= s_ControlBits(1);
 	Shift_LeftRight_Signal_Out <= s_ControlBits(0);
 end Design;
+
+--ALU input
+--add = 0001
+--and = 0010
+--nor = 0011
+--xor = 0100
+--or = 0101
+--slt = 0110
+--sll = 0111
+--srl = 1000
+--sra = 1001
+--sub = 1010
+--lui = 1101
+--lw/sw = 0000
+--beq = 1011
+--bne = 1100
+--jumps = 1110
