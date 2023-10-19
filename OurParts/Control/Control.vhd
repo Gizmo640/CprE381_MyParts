dcfc92621aadbb56fc735c38eaab67bd2958991b
lincolnh <lincolnh@iastate.edu>
2023-10-19 00:45:00 -0500

    finished control

--- OurParts/Control/Control.vhd
+++ OurParts/Control/Control.vhd
@@ -16,7 +16,7 @@ entity Control is
         MemRead: out std_logic;  --bit 4
         MemWrite: out std_logic; --bit 5
         MemtoReg: out std_logic; --bit 6
-        ALUOp: out std_logic_vector(1 downto 0); --bit 8, bit 7
+        ALUOp: out std_logic_vector(3 downto 0); --bit 8, bit 7
         ALUSrc: out std_logic;   --bit 9
         RegWrite: out std_logic; --bit 10
         RegDst: out std_logic;  --bit 11
@@ -51,30 +51,22 @@ architecture dataflow of Control is
     '1' when (Opcode = "100011") else '0'; --lw
 
     ALUOp <=
-    "00000" when ((Opcode = "000000") and (Funct = "100001")) else  --addu
-    "00000" when ((Opcode = "000000") and (Funct = "100000")) else  --add
-    "00001" when ((Opcode = "000000") and (Funct = "100100")) else  --and
-    "00010" when ((Opcode = "000000") and (Funct = "100111")) else  --nor
-    "00011" when ((Opcode = "000000") and (Funct = "100110")) else  -- xor
-    "00100" when ((Opcode = "000000") and (Funct = "100101")) else  -- or
-    "00101" when ((Opcode = "000000") and (Funct = "101010")) else  --slt
-    "00110" when ((Opcode = "000000") and (Funct = "000000")) else  -- sll 
-    "00111" when ((Opcode = "000000") and (Funct = "000010")) else  -- srl 
-    "01000" when ((Opcode = "000000") and (Funct = "000011")) else  -- sra
-    "01001" when ((Opcode = "000000") and (Funct = "100010")) else  -- sub
-    "01001" when ((Opcode = "000000") and (Funct = "100011")) else  -- subu
-    "01010" when (Opcode = "001000") else   -- addi
-    "01010" when (Opcode = "001001") else   -- addiu
-    "01011" when (Opcode = "001100") else   --andi
-    "01100" when (Opcode = "001111") else   -- lui
-    "01101" when (Opcode = "100011") else   -- lw
-    "01101" when (Opcode = "101011") else   -- sw
-    "01110" when (Opcode = "001110") else   -- xori
-    "01111" when (Opcode = "001010") else   -- slti
-    "10000" when (Opcode = "001101") else   -- ori
-    "10001" when (Opcode = "000100") else   -- beq
-    "10001" when (Opcode = "000101") else   -- bne
-    "xxxxx"; --useless
+    "0000" when ((Opcode = "000000") and (Funct = "100000")) else  --add
+    "0001" when ((Opcode = "000000") and (Funct = "100001")) else  --addu
+    "0010" when ((Opcode = "000000") and (Funct = "100100")) else  --and
+    "0100" when ((Opcode = "000000") and (Funct = "100111")) else  --nor
+    "0101" when ((Opcode = "000000") and (Funct = "100110")) else  -- xor
+    "0011" when ((Opcode = "000000") and (Funct = "100101")) else  -- or
+    "0110" when ((Opcode = "000000") and (Funct = "101010")) else  --slt
+    "0111" when ((Opcode = "000000") and (Funct = "000000")) else  -- sll 
+    "1000" when ((Opcode = "000000") and (Funct = "000010")) else  -- srl 
+    "1001" when ((Opcode = "000000") and (Funct = "000011")) else  -- sra
+    "1010" when ((Opcode = "000000") and (Funct = "100010")) else  -- sub
+    "1011" when ((Opcode = "000000") and (Funct = "100011")) else  -- subu
+    "1100" when (Opcode = "001111") else   -- lui
+    "1101" when (Opcode = "100011") else   -- lw
+    "1110" when (Opcode = "000100") else   -- beq
+    "xxxx"; --useless
 
     ALUSrc <=
     '0' when (Opcode = "000000") else
