ecdea17b3e7aa7a1b3f6e5c3a3a2031e5a95c058
lincolnh <lincolnh@iastate.edu>
2023-10-19 00:10:06 -0500

    made some changes to Control

--- OurParts/Control/Control.vhd
+++ OurParts/Control/Control.vhd
@@ -8,7 +8,7 @@ use IEEE.std_logic_1164.all;
 entity Control is
     port(
         Opcode: in std_logic_vector(5 downto 0);
-      --  ControlOut: std_logic_vector(11 downto 0);
+        Funct: in std_logic_vector(5 downto 0);
         Jump: out std_logic; --bit 0
         Jr: out std_logic;   --bit 1 (does jr need to be an ALU control sig? It depends on the funct code)
         Branch: out std_logic;   --bit 2
@@ -29,10 +29,10 @@ architecture dataflow of Control is
     begin
 
     Jump <=
-    '1' when (Opcode = "000010") else '0';
+    '1' when (Opcode = "000010") else '0'; --jump
 
     Jr <= --how to do for jr
-    '1' when (Opcode = "000000") else '0'; --rtype
+    '1' when ((Opcode = "000000") and (Funct = "001000")) else '0'; --rtype
 
     Branch <=
     '1' when (Opcode = "000010") else --beq
@@ -51,13 +51,31 @@ architecture dataflow of Control is
     '1' when (Opcode = "100011") else '0'; --lw
 
     ALUOp <=
-    "10" when (Opcode = "000000") else  --rtype
-    "00" when (Opcode = "100011") else  --lw
-    "00" when (Opcode = "101011") else  --sw
-    "01" when (Opcode = "000100") else --beq
-    "01" when (Opcode = "000101") else --bne
-    "11"; --for I type (how do we choose which function?)
-    
+    "00000" when ((Opcode = "000000") and (Funct = "100001")) else  --addu
+    "00000" when ((Opcode = "000000") and (Funct = "100000")) else  --add
+    "00001" when ((Opcode = "000000") and (Funct = "100100")) else  --and
+    "00010" when ((Opcode = "000000") and (Funct = "100111")) else  --nor
+    "00011" when ((Opcode = "000000") and (Funct = "100110")) else  -- xor
+    "00100" when ((Opcode = "000000") and (Funct = "100101")) else  -- or
+    "00101" when ((Opcode = "000000") and (Funct = "101010")) else  --slt
+    "00110" when ((Opcode = "000000") and (Funct = "000000")) else  -- sll 
+    "00111" when ((Opcode = "000000") and (Funct = "000010")) else  -- srl 
+    "01000" when ((Opcode = "000000") and (Funct = "000011")) else  -- sra
+    "01001" when ((Opcode = "000000") and (Funct = "100010")) else  -- sub
+    "01001" when ((Opcode = "000000") and (Funct = "100011")) else  -- subu
+    "01010" when (Opcode = "001000") else   -- addi
+    "01010" when (Opcode = "001001") else   -- addiu
+    "01011" when (Opcode = "001100") else   --andi
+    "01100" when (Opcode = "001111") else   -- lui
+    "01101" when (Opcode = "100011") else   -- lw
+    "01101" when (Opcode = "101011") else   -- sw
+    "01110" when (Opcode = "001110") else   -- xori
+    "01111" when (Opcode = "001010") else   -- slti
+    "10000" when (Opcode = "001101") else   -- ori
+    "10001" when (Opcode = "000100") else   -- beq
+    "10001" when (Opcode = "000101") else   -- bne
+    "xxxxx"; --useless
+
     ALUSrc <=
     '0' when (Opcode = "000000") else
     '0' when (Opcode = "000100") else
@@ -65,18 +83,18 @@ architecture dataflow of Control is
     '0' when (Opcode = "000010") else
     '0' when (Opcode = "000011") else '1';
 
-    RegWrite <= --what do we do for jr???
+    RegWrite <= 
     '0' when (Opcode = "101011") else --sw
     '0' when (Opcode = "000100") else --beq
     '0' when (Opcode = "000101") else --bne
     '0' when (Opcode = "000010") else --j
-    '0' when (Opcode = "000000") else '1'; --we only want to set this for jr
+    '0' when ((Opcode = "000000") and (Funct = "001000")) else '1'; --we only want to set this for jr
 
 
     RegDst <=
     '1' when (Opcode = "000000") else '0'; --rtype
 
     Halt <=
-    '1' when (Opcode = "010100") else '0';
+    '1' when (Opcode = "010100") else '0'; --single signal
 
 end dataflow;
